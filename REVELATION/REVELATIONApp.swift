f11e5f9dba6be9e0a1d5619005684ac1e4a0b9e1
FangTheWerewolf <cfls3264@gmail.com>
2025-01-30 15:26:38 -0500

    Initial Commit

---
+++ REVELATION/REVELATIONApp.swift
@@ -0,0 +1,17 @@
+//
+//  REVELATIONApp.swift
+//  REVELATION
+//
+//  Created by Christopher on 1/30/25.
+//
+
+import SwiftUI
+
+@main
+struct REVELATIONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
